28da7e20e970609e939c99ad12b7ed20e6906e5a
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-03 12:58:00 -0400

    f-2 | Added path for login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
 
 const routes: Routes = [
-  {path: 'register', component: RegisterPageComponent  }
+  {path: 'register', component: RegisterPageComponent },
+  {path: 'login', component: LoginPageComponent }
 ];
 
 @NgModule({
