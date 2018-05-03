d5a79b1924873806ec2053d5d4b0f68618ac34b5
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-03 10:17:34 -0400

    f-1 | added route for register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterPageComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
