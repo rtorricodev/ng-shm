c24c513e4d039de432d5f0d5cd39d83ff555fe1a
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-03 10:17:02 -0400

    f-1 | imported modules and componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+//forms
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+//botstrap
+import {AlertModule} from "ngx-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+//page components
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+//components
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+
+//services
+import { AuthService } from './services/auth.service';
+
+//firebase imports
+import { environment } from './../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertModule.forRoot(),
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig , 'SHM web'),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
