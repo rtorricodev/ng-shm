92765bd91f519f02817f901ca177c2745ff8a094
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-03 18:08:25 -0400

    f-2 | added loging component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'register', component: RegisterPageComponent },
-  {path: 'home', component: HomePageComponent}
+  {path: 'home', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
