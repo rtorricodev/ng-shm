c0b4f50a96e8bf3829459dbac196febf672b6534
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-03 17:07:30 -0400

    f-2 | implemented auto redirection when a user is registered

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import { AuthService } from './../../services/auth.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register-page',
@@ -9,11 +10,16 @@ import { AuthService } from './../../services/auth.service';
 })
 export class RegisterPageComponent {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   isLoggedIn(): Observable<boolean> {
     return this.authService.isLoggedIn();
   }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
+  }
+
 
 
 }
