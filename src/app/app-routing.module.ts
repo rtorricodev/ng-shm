c4d1faa27140ed30f95ab88a83a2c3956f10adc4
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-05-09 01:46:22 -0400

    t-2 | deleted login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'register', component: RegisterPageComponent },
   {path: 'home', component: HomePageComponent},
-  {path: 'login', component: LoginPageComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
